9291a4091682cf509aabc35639273f9f09827714
Vinícius Nunes <99284437+buttowsk@users.noreply.github.com>
2022-10-25 20:28:10 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
     function settarUsuarioAtual () {
-        //let id = Math.floor(Math.random() * 30)
+        //let id = Math.floor(Math.random() * 31)
         let id = 24
         const iconeUser = document.getElementById('fotoUsuario')
 
